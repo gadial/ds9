4757db55b9cb26c1528a4ec2035e48914baae555
Gadi Aleksandrowicz <gadial@gmail.com>
2019-07-09 16:41:26 +0300

    Added the rest of the reviews

---
+++ _reviews/temp.rb
@@ -0,0 +1,4 @@
+Dir.glob("S0[4-7]E*.markdown") do |filename|
+    File.read(filename) =~ /name: \"([^\"]+)\"/
+    File.rename(filename,"#{File.basename(filename,".markdown")}-#{$1.gsub(".","").gsub(" ", "-")}.markdown")
+end
